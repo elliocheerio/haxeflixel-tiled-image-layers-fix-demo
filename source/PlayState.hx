d7ad2c2b186243feb86cc4822af8e3829e384bb3
ElliotRC <elliotrc129@gmail.com>
2018-12-11 23:27:44 -0500

    initial commit

---
+++ source/PlayState.hx
@@ -0,0 +1,78 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flixel.group.FlxGroup;
+import flash.system.System;
+import flash.Lib;
+
+class PlayState extends FlxState
+{
+	public var player:FlxSprite;
+	public var coins:FlxGroup;
+
+	override public function create():Void
+	{
+		super.create();
+
+		FlxG.log.redirectTraces = true;
+
+		var level = new TiledLevel("assets/level.tmx", this);
+
+		player = new FlxSprite("assets/player.png");
+		player.x = 80;
+		player.y = 80;
+
+		add(level.backgroundLayer);
+
+		add(level.imagesLayer);
+
+		add(player);
+
+		FlxG.camera.follow(player, LOCKON);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		input();
+	}
+
+	private function input():Void
+	{
+		var velocity:Int = 60;
+
+		player.velocity.x = 0;
+		player.velocity.y = 0;
+
+		if (FlxG.keys.anyPressed([LEFT, A]))
+		{
+			player.velocity.x -= velocity;
+		}
+		if (FlxG.keys.anyPressed([RIGHT, D]))
+		{
+			player.velocity.x += velocity;
+		}
+		if (FlxG.keys.anyPressed([W, UP]))
+		{
+			player.velocity.y -= velocity;
+		}
+		if (FlxG.keys.anyPressed([S, DOWN]))
+		{
+			player.velocity.y += velocity;
+		}
+
+		// quit
+		if (FlxG.keys.anyPressed([ESCAPE]))
+		{
+			#if !flash
+			System.exit(0);
+			#else
+			Lib.fscommand("quit");
+			#end
+		}
+	}
+}
---
+++ source/TiledLevel.hx
@@ -0,0 +1,109 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.addons.editors.tiled.TiledLayer;
+import flixel.addons.editors.tiled.TiledImageLayer;
+import flixel.addons.editors.tiled.TiledImageTile;
+import flixel.addons.editors.tiled.TiledLayer.TiledLayerType;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.addons.editors.tiled.TiledTileSet;
+import flixel.addons.editors.tiled.TiledTilePropertySet;
+import flixel.group.FlxGroup;
+import flixel.tile.FlxTilemap;
+import flixel.addons.tile.FlxTilemapExt;
+import flixel.addons.tile.FlxTileSpecial;
+import haxe.io.Path;
+
+/**
+ * @author Samuel Batista
+ */
+class TiledLevel extends TiledMap
+{
+	// For each "Tile Layer" in the map, you must define a "tileset" property which contains the name of a tile sheet image 
+	// used to draw tiles in that layer (without file extension). The image file must be located in the directory specified bellow.
+	inline static var c_PATH_LEVEL_TILESHEETS = "assets/";
+	
+	public var backgroundLayer:FlxGroup;
+	public var imagesLayer:FlxGroup;
+	public var coins:FlxGroup;
+	var collidableTileLayers:Array<FlxTilemap>;
+	
+	public var player:FlxSprite;
+	
+	public function new(tiledLevel:FlxTiledMapAsset, state:PlayState)
+	{
+		super(tiledLevel);
+		
+		imagesLayer = new FlxGroup();
+		backgroundLayer = new FlxGroup();
+		coins = new FlxGroup();
+		
+		FlxG.camera.setScrollBoundsRect(0, 0, fullWidth, fullHeight, true);
+		
+		loadTiledLayers();
+	}
+
+	public function loadTiledLayers():Void
+	{
+		for (layer in layers)
+		{
+			switch (layer.type)
+			{
+				case TiledLayerType.TILE:
+					loadLayerMap(layer);
+				case TiledLayerType.IMAGE:
+					loadLayerImages(layer);
+				default:
+					// no layer type. do nothing
+			}
+		}
+	}
+
+	public function loadLayerMap(layer:TiledLayer):Void
+	{
+			var tileLayer:TiledTileLayer = cast layer;
+			
+			var tileSheetName:String = tileLayer.properties.get("tileset");
+			
+			if (tileSheetName == null)
+				throw "'tileset' property not defined for the '" + tileLayer.name + "' layer. Please add the property to the layer.";
+				
+			var tileSet:TiledTileSet = null;
+			for (ts in tilesets)
+			{
+				if (ts.name == tileSheetName)
+				{
+					tileSet = ts;
+					break;
+				}
+			}
+			
+			if (tileSet == null)
+				throw "Tileset '" + tileSheetName + " not found. Did you misspell the 'tilesheet' property in " + tileLayer.name + "' layer?";
+				
+			var imagePath 	  = new Path(tileSet.imageSource);
+			var processedPath = c_PATH_LEVEL_TILESHEETS + imagePath.file + "." + imagePath.ext;
+			
+			// could be a regular FlxTilemap if there are no animated tiles
+			var tilemap = new FlxTilemapExt();
+			tilemap.loadMapFromArray(tileLayer.tileArray, width, height, processedPath,
+				tileSet.tileWidth, tileSet.tileHeight, OFF, tileSet.firstGID, 1, 1);
+			
+			backgroundLayer.add(tilemap);
+
+			if (collidableTileLayers == null)
+				collidableTileLayers = new Array<FlxTilemap>();
+				
+			collidableTileLayers.push(tilemap);
+	}
+
+	public function loadLayerImages(layer:TiledLayer)
+	{
+		var image:TiledImageLayer = cast layer;
+		var sprite = new FlxSprite(image.x, image.y, c_PATH_LEVEL_TILESHEETS + image.imagePath);
+		imagesLayer.add(sprite);
+	}
+}
